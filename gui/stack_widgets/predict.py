4a19b2578cc46caa352a1b401d9ab3dba6975c52
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-15 12:56:19 -0500

    Delete gui directory

--- gui/stack_widgets/predict.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget
-
-class Predict(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        layout = QVBoxLayout()
-        self.setLayout(layout)
